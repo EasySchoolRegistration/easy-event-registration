ba59579b213ac58e82ae8e556347ab86601b35dc
Zbynek Nedoma <nedoma@forturio.cz>
2018-06-01 13:20:10 +0200

    Show wrapper arround all tickets

--- inc/template/event_sale/eer-event-sale.template.php
+++ inc/template/event_sale/eer-event-sale.template.php
@@ -13,18 +13,19 @@ class EER_Template_Event_Sale
 		ob_start();
 
 		// check event sale started
+
+		echo '<div class="eer-tickets-sale-wrapper eer-event-' . $event_id . '">';
 		if (EER()->event->is_event_sale_active($event_id)) {
 			$templater_tickets = new EER_Template_Event_Sale_Tickets();
 			$templater_user_form = new EER_Template_Event_Sale_User_Form();
 
-			echo '<div class="eer-tickets-sale-wrapper">';
 			$templater_tickets->print_content($event_id);
 			$templater_user_form->print_content($event_id);
-			echo '</div>';
 		} else {
 			$templater_not_opened = new EER_Template_Event_Sale_Not_Opened();
 			$templater_not_opened->print_content($event_id);
 		}
+		echo '</div>';
 
 		echo ob_get_clean();
 	}
