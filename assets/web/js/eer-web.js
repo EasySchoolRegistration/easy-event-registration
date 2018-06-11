8f3dd5dbef4db4fbf257e05acce12b758e180a18
Zbynek Nedoma <domaneni@gmail.com>
2018-06-11 07:33:42 +0200

    Added wrapper

--- assets/web/js/eer-web.js
+++ assets/web/js/eer-web.js
@@ -11,11 +11,12 @@ jQuery(function ($) {
 		/** global: ajaxurl */
 		$("body").on("click", ".eer-ticket:not(.eer-ticket-remove):not(.eer-sold) .eer-ticket-add", function (e) {
 			e.preventDefault();
+			var sale_wrapper = $(this).closest(".eer-tickets-sale-wrapper");
 			var ticket = $(this).closest(".eer-ticket");
 
 			if ($("#eer-ticket-" + ticket.data("id")).length === 0) {
-				var prep_row = $(".eer-ticket-default-form-row").html();
-				var tickets_placeholder = $(".eer-form-tickets");
+				var prep_row = sale_wrapper.find(".eer-ticket-default-form-row").html();
+				var tickets_placeholder = sale_wrapper.find(".eer-form-tickets");
 
 				if (tickets_placeholder.hasClass("eer-error")) {
 					tickets_placeholder.removeClass("eer-error").empty();
@@ -24,13 +25,13 @@ jQuery(function ($) {
 				//Replace data in prep row
 				prep_row = prep_row.replace(/%eer-ticket-title%/g, ticket.data("title"));
 				prep_row = prep_row.replace(/%eer-price%/g, ticket.data("price"));
-				prep_row = prep_row.replace(/%eer-price-currency%/g, eer_price_template(ticket.data("price")));
+				prep_row = prep_row.replace(/%eer-price-currency%/g, eer_price_template(ticket.data("price"), sale_wrapper));
 				prep_row = prep_row.replace(/%eer-max%/g, ticket.data("max"));
 				prep_row = prep_row.replace(/%eer-ticket-id%/g, ticket.data("id"));
 				prep_row = prep_row.replace(/%eer-separately%/g, ticket.data("sold_separately"));
 				tickets_placeholder.append(prep_row);
 
-				var ticket_row = $(".eer-form-tickets #eer-ticket-" + ticket.data("id"));
+				var ticket_row = sale_wrapper.find(".eer-form-tickets #eer-ticket-" + ticket.data("id"));
 				if (ticket.data("max") === 1) {
 					ticket_row.find(".eer-number-of-tickets").hide();
 				}
@@ -59,18 +60,19 @@ jQuery(function ($) {
 				}
 
 				if (ticket.data("sold_separately") === 1) {
-					$(".eer-tickets .eer-ticket[data-sold_separately=1]").addClass("eer-ticket-remove");
+					sale_wrapper.find(".eer-tickets .eer-ticket[data-sold_separately=1]").addClass("eer-ticket-remove");
 				}
 
 				ticket.addClass("eer-ticket-remove");
 
-				eer_tickets_final_price_recount();
+				eer_tickets_final_price_recount(sale_wrapper);
 			}
 		}).on("click", ".eer-ticket-remove", function () {
+			var sale_wrapper = $(this).closest(".eer-tickets-sale-wrapper");
 			var ttb = $(this).closest(".eer-ticket-to-buy");
-			var ticket = $(".eer-ticket[data-id=" + ttb.data("id") + "]").removeClass("eer-ticket-remove");
+			var ticket = sale_wrapper.find(".eer-ticket[data-id=" + ttb.data("id") + "]").removeClass("eer-ticket-remove");
 			ttb.remove();
-			eer_tickets_final_price_recount();
+			eer_tickets_final_price_recount(sale_wrapper);
 
 			if (ticket.data("sold_separately") === 1) {
 				$(".eer-tickets .eer-ticket[data-sold_separately=1]").removeClass("eer-ticket-remove");
@@ -79,7 +81,7 @@ jQuery(function ($) {
 			if (parseInt($(this).val()) > parseInt($(this).attr("max"))) {
 				$(this).val(parseInt($(this).attr("max")));
 			}
-			eer_tickets_price_recount_row($(this).closest(".eer-ticket-to-buy"));
+			eer_tickets_price_recount_row($(this).closest(".eer-ticket-to-buy"), $(this).closest(".eer-tickets-sale-wrapper"));
 		}).on("change", "input.choose_partner", function () {
 			if ($(this).is(":checked")) {
 				var partner_input_box = $(this).closest(".eer-partner").find(".eer-info-row.dancing-with-email");
@@ -117,11 +119,12 @@ jQuery(function ($) {
 				}
 			}
 		}).on("change", ".eer-update-price", function () {
-			eer_tickets_final_price_recount();
+			eer_tickets_final_price_recount($(this).closest(".eer-tickets-sale-wrapper"));
 		}).on("submit", "#eer-ticket-shop-form", function (e) {
 			e.preventDefault();
 			var form = $(this);
-			$(".eer-error:not(.eer-form-tickets)").remove(); //Remove old errors
+			var sale_wrapper = $(this).closest(".eer-tickets-sale-wrapper");
+			form.find(".eer-error:not(.eer-form-tickets)").remove(); //Remove old errors
 			var order_data = {};
 
 			order_data["tickets"] = {};
@@ -171,18 +174,18 @@ jQuery(function ($) {
 				$.post(eer_ajax_object.ajaxurl, data, function (response) {
 				}).done(function (response) {
 					if (response.hasOwnProperty("thank_you_text")) {
-						$(".eer-ticket-shop-form").remove();
-						$(".eer-tickets").empty().append(response.thank_you_text);
+						sale_wrapper.find(".eer-ticket-shop-form").remove();
+						sale_wrapper.find(".eer-tickets").empty().append(response.thank_you_text);
 					} else if (response.hasOwnProperty("errors")) {
 						$.each(response.errors.errors, function (key, message) {
 							var keys = key.split(".");
 							if (keys[0] === "user_info") {
-								$("[name=" + keys[1] + "]").after("<div class=\"eer-error\">" + message[0] + "</div>");
+								sale_wrapper.find("[name=" + keys[1] + "]").after("<div class=\"eer-error\">" + message[0] + "</div>");
 							} else if (keys[0] === "tickets") {
 								if (keys[1] === "all") {
 
 								} else {
-									$(".eer-ticket-to-buy[data-id=" + keys[1] + "]").append("<div class=\"eer-error\">" + message[0] + "</div>");
+									sale_wrapper.find(".eer-ticket-to-buy[data-id=" + keys[1] + "]").append("<div class=\"eer-error\">" + message[0] + "</div>");
 								}
                                 if (keys[2] === "full") {
                                     //$(".eer-ticket[data-id=" + keys[1] + "]").addClass('eer-sold');
@@ -190,7 +193,7 @@ jQuery(function ($) {
 							}
 						});
 						$('html, body').animate({
-							scrollTop: $(".eer-form-tickets").offset().top - 50
+							scrollTop: sale_wrapper.find(".eer-form-tickets").offset().top - 50
 						}, 2000);
 					}
 				});
@@ -199,19 +202,19 @@ jQuery(function ($) {
 			}
 		});
 
-		function eer_tickets_price_recount_row(row) {
+		function eer_tickets_price_recount_row(row, sale_wrapper) {
 			var number_of_tickets = $(row).find(".eer-number-of-tickets input[type=number]").val();
 			var price_row = $(row).find(".eer-price");
 			var price = price_row.data("price");
 
-			$(row).find(".eer-price").text(eer_price_template((isNaN(price) ? 0 : price) * (isNaN(number_of_tickets) ? 1 : number_of_tickets)));
-			eer_tickets_final_price_recount();
+			$(row).find(".eer-price").text(eer_price_template((isNaN(price) ? 0 : price) * (isNaN(number_of_tickets) ? 1 : number_of_tickets), sale_wrapper));
+			eer_tickets_final_price_recount(sale_wrapper);
 		}
 
-		function eer_tickets_final_price_recount() {
+		function eer_tickets_final_price_recount(sale_wrapper) {
 			var final_sum = 0;
 
-			$(".eer-ticket-shop-form .eer-ticket-to-buy").each(function () {
+			sale_wrapper.find(".eer-ticket-shop-form .eer-ticket-to-buy").each(function () {
 				var number_of_tickets = $(this).find(".eer-number-of-tickets input[type=number]").val();
 
 				var price_row = $(this).find(".eer-price");
@@ -219,17 +222,17 @@ jQuery(function ($) {
 				final_sum += (isNaN(price) ? 0 : price) * (isNaN(number_of_tickets) ? 1 : number_of_tickets);
 			});
 
-			$(".eer-update-price").each(function () {
+			sale_wrapper.find(".eer-update-price").each(function () {
 				var price = $(this).find("option:selected").data("price");
 				final_sum += isNaN(price) ? 0 : price;
 			});
 
-			var finalPrice = $(".eer-final-price .eer-final-price-value");
-			finalPrice.text(eer_price_template(final_sum));
+			var finalPrice = sale_wrapper.find(".eer-final-price .eer-final-price-value");
+			finalPrice.text(eer_price_template(final_sum, sale_wrapper));
 		}
 
-		function eer_price_template(price) {
-			var price_template = $(".eer-form-tickets").data("price-template");
+		function eer_price_template(price, sale_wrapper) {
+			var price_template = sale_wrapper.find(".eer-form-tickets").data("price-template");
 			return price_template.replace("[price]", price);
 		}
 	});
